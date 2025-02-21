99fc516afa3d23df519125ac376a31eb02766074
Vlad <mrAlheor@gmail.com>
2025-02-21 13:00:47 +0400

    iter1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/Alheor/shorturl/internal/controller"
+	"github.com/Alheor/shorturl/internal/repository"
+)
+
+func main() {
+
+	repository.Init()
+
+	err := http.ListenAndServe(controller.Addr, controller.GetRouter())
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Alheor/shorturl
+
+go 1.22.12
