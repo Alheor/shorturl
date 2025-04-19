01eacfb6d7108dd0a1cbe619aa62d4a6fe9c3233
Vlad <mrAlheor@gmail.com>
2025-04-19 12:20:08 +0300

    iter14 tmp

--- internal/migrations/00001_create_table.sql
+++ internal/migrations/00001_create_table.sql
@@ -1,11 +1,11 @@
 -- +goose Up
-CREATE TABLE IF NOT EXISTS public.short_url (
+CREATE TABLE public.short_url (
 	id SERIAL NOT NULL PRIMARY KEY,
 	short_key varchar(8) UNIQUE NOT NULL,
 	original_url text NOT NULL
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS short_url_original_url_unique_idx ON  public.short_url (original_url);
+CREATE UNIQUE INDEX short_url_original_url_unique_idx ON  public.short_url (original_url);
 
 -- +goose Down
 DROP INDEX short_url_original_url_unique_idx;
