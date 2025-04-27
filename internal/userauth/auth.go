d2f7f121132859b85b06c4b8f3858cfbb4f95cd7
Vlad <mrAlheor@gmail.com>
2025-04-27 10:52:39 +0300

    iter15 review fix

--- internal/userauth/auth.go
+++ internal/userauth/auth.go
@@ -34,14 +34,16 @@ func AuthHTTPHandler(f http.HandlerFunc) http.HandlerFunc {
 	return func(resp http.ResponseWriter, req *http.Request) {
 
 		var userCookie *models.UserCookie
+		var cookie *http.Cookie
 		var err error
 
-		for _, cookie := range req.Cookies() {
-			if cookie.Name == models.CookiesName {
-				userCookie, err = parseCookie(cookie)
-				break
-			}
+		cookie, err = req.Cookie(models.CookiesName)
+		if err != nil || cookie == nil {
+			f(resp, req)
+			return
 		}
+
+		userCookie, err = parseCookie(cookie)
 
 		if err != nil {
 			var myErr *models.EmptyUserIDErr
