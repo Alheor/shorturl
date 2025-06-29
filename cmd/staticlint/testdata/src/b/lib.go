52fb3dccd8a02a6127ce1fc51a4444c894c78f44
Vlad <mrAlheor@gmail.com>
2025-06-29 12:11:05 +0300

    iter19 tests fix

--- cmd/staticlint/testdata/src/b/lib.go
+++ cmd/staticlint/testdata/src/b/lib.go
@@ -1,13 +1,14 @@
+// Package lib Данные для тестирования multichecker-а
 package lib
 
 import "os"
 
-// В пакете не main анализатор не должен срабатывать
+// SomeFunc В пакете не main анализатор не должен срабатывать
 func SomeFunc() {
 	os.Exit(0) // OK - не в пакете main
 }
 
+// SomeFunc Даже если есть функция main в пакете не main, анализатор не должен срабатывать
 func main() {
-	// Даже если есть функция main в пакете не main
 	os.Exit(1) // OK - не в пакете main
 }
--- cmd/staticlint/testdata/src/c/util.go
+++ cmd/staticlint/testdata/src/c/util.go
@@ -1,12 +1,14 @@
+// Данные для тестирования multichecker-а
 package main
 
 import "os"
 
-// Пакет main, но без функции main - не должно быть проверок
+// HelperFunction Пакет main, но без функции main - анализатор не должен срабатывать
 func HelperFunction() {
 	os.Exit(0) // OK - не в функции main
 }
 
+// AnotherHelper Пакет main, но без функции main - анализатор не должен срабатывать (дополнительная вложенность)
 func AnotherHelper() {
 	if true {
 		os.Exit(1) // OK - не в функции main
